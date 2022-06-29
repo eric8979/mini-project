877fd36b6f1aaf1f825e984b86e252a1351fcdf3
Eric Whale <hsh048148@gmail.com>
2022-06-29 19:24:29 +0900

    Fix hackernews.py-use only urllib

--- hackernews.py
+++ hackernews.py
@@ -1,5 +1,6 @@
-# Crawl Hackernews links with certain topic (first page) (regex)
-import socket
+# Description
+# Crawl Hackernews links with certain topic (first page)
+
 import ssl
 import urllib.request
 import urllib.parse
@@ -16,22 +17,3 @@ for atag in atags:
 
 for i in range(len(newsData)):
     print(str(i) + " : " + newsData[i][0] + " - " + newsData[i][1])
-
-# ---------------------------------------
-
-# mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# mysocket.connect(('news.ycombinator.com', 443))
-# request = 'GET https://news.ycombinator.com HTTP/1.0\n\n'.encode()
-# mysocket.send(request)
-
-# while True:
-#     try:
-#         raw = mysocket.recv(1024)
-#         if len(raw) < 1:
-#             break
-#         print(raw.decode())
-#     except:
-#         print('failed to receive info')
-#         break
-
-# mysocket.close()
